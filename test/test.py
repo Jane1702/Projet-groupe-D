e8c40504b7da32939cdf07733e0ce827367f00d8
Chaïmae <chaimaelyesri8@gmail.com>
2023-10-26 12:38:11 +0200

    3->III

--- test/test.py
+++ test/test.py
@@ -1,20 +1,35 @@
 import unittest
-import parameterized.parameterized
 
 from ConvertisseurNombresRomains import ConvertisseurNombresRomains
 
 
 class NombresRomainsTest(unittest.TestCase):
+    def test_un(self):
+        # ETANT DONNE le chiffre 1
+        nombre_arabe = 1
 
-    @parameterized.parameterized.expand([
-        [1],
-        [2],
-    ])
-    def test_unite(self, nombre_arabe):
-        # ETANT DONNE le chiffre <nombre_arabe>
         # QUAND on le convertit en nombres romains
         nombre_romain = ConvertisseurNombresRomains.convertir(nombre_arabe)
 
-        # ALORS on obtient "I" répété <nombre_arabe> fois
-        attendu = "I" * nombre_arabe
-        self.assertEqual(attendu, nombre_romain)
+        # ALORS on obtient "I"
+        self.assertEqual("I", nombre_romain)
+
+    def test_deux(self):
+        # ETANT DONNE le chiffre 2
+        nombre_arabe = 2
+
+        # QUAND on le convertit en nombres romains
+        nombre_romain = ConvertisseurNombresRomains.convertir(nombre_arabe)
+
+        # ALORS on obtient "II"
+        self.assertEqual("II", nombre_romain)
+
+    def test_trois(self):
+        # ETANT DONNE le chiffre 3
+        nombre_arabe = 3
+
+        # QUAND on le convertit en nombres romains
+        nombre_romain = ConvertisseurNombresRomains.convertir(nombre_arabe)
+
+        # ALORS on obtient "III"
+        self.assertEqual("III", nombre_romain)
