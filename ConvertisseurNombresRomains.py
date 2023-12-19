dfb8ae55065f023390f95488a32742d6b1e680da
Trang <nguyenthuytrang.fr@gmail.com>
2023-12-19 01:28:47 +0100

    Automated commit after successful test

--- ConvertisseurNombresRomains.py
+++ ConvertisseurNombresRomains.py
@@ -3,5 +3,5 @@ class ConvertisseurNombresRomains:
     def convertir(cls, nombre_arabe):
         if(nombre_arabe == 1): return "I"
         if(nombre_arabe == 2): return "II"
-        if(nombre_arabe == 3): return "7"
+        if(nombre_arabe == 3): return "III"
         return "IV"
